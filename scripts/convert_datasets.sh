efe1d310158cbd21f1b368c3034849a4bf125345
anapimenta1 <83237743+anapimenta1@users.noreply.github.com>
2023-04-12 02:04:29 +0100

    Update convert_datasets.sh

--- scripts/convert_datasets.sh
+++ scripts/convert_datasets.sh
@@ -1,18 +1,3 @@
-python3 -m src.dataset.create_records --records_root /ssd/dataset/histology/converted/ --data_root /ssd/dataset/histology/original/Breast\ cancer/ --name breakhis
-
-
-python3 src/dataset/create_records.py --records_root /ssd/dataset/histology/converted/ --data_root /ssd/dataset/histology/original/Breast\ cancer/ --name bach
-
-
-python3 src/dataset/create_records.py --records_root /ssd/dataset/histology/converted/ --data_root /ssd/dataset/histology/original/Colorectal\ cancer/ --name crc-tp
-
-
-python3 src/dataset/create_records.py --records_root /ssd/dataset/histology/converted/ --data_root /ssd/dataset/histology/original/Colorectal\ cancer/ --name kaggle_crc
-
-
-python3 src/dataset/create_records.py --records_root /ssd/dataset/histology/converted/ --data_root /ssd/dataset/histology/original/Colorectal\ cancer/ --name lc25000
-
-
-python3 src/dataset/create_records.py --records_root /ssd/dataset/histology/converted/ --data_root /ssd/dataset/histology/original/Colorectal\ cancer/ --name nct
+python3 -m src.dataset.create_records --records_root /home/guests2/apa/datasets_converted --data_root /home/guests2/apa/datasets/breakhis --name breakhis
 
 
