ff16bad29e8704abc02b42e88cba89dfbdf78cd2
anapimenta1 <83237743+anapimenta1@users.noreply.github.com>
2023-06-17 16:49:06 +0100

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,21 +1,19 @@
 method=$1
-train=$2
-train2=$3
-valid=$4
-test=$5
+train1="crc-tp"
+train2="nct"
+test="lc25000"  # Choose the desired test dataset
 
 seeds="[2021]"
-train="[$train, $train2]"
-valid="[\'$valid\']"
-test="[\'$test\']"
+train="['$train1', '$train2']"
+valid="[]"  # You can specify validation sources if needed
+test="['$test']"
 
 base_config_path="config/base.yaml"
 method_config_path="config/${method}.yaml"
 
 python3 -m src.train --base_config ${base_config_path} \
                      --method_config ${method_config_path} \
-                     --opts "train_sources" $train \
-                            "seeds" $seeds \
-                            "val_sources" $valid \
-                            "test_sources" $test
-
+                     --opts train_sources ${train} \
+                            seeds ${seeds} \
+                            val_sources ${valid} \
+                            test_sources ${test}
