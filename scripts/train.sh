bbf0df52c3820ebc72947e28a83390406001ff98
anapimenta1 <83237743+anapimenta1@users.noreply.github.com>
2023-06-17 16:44:41 +0100

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,15 +6,15 @@ test=$5
 
 seeds="[2021]"
 train="[$train, $train2]"
-valid="['$valid']"
-test="['$test']"
+valid="[$valid]"
+test="[$test]"
 
 base_config_path="config/base.yaml"
 method_config_path="config/${method}.yaml"
 
 python3 -m src.train --base_config ${base_config_path} \
                      --method_config ${method_config_path} \
-                     --opts train_sources ${train} \
-                            seeds ${seeds} \
-                            val_sources ${valid} \
-                            test_sources ${test} \
+                     --opts train_sources $train \
+                            seeds $seeds \
+                            val_sources $valid \
+                            test_sources $test
